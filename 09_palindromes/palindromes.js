f1be6e0e849364a99e69bba44aa145a86092f326
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-05-08 01:26:54 -0400

    Exercise 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanWord = word
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumeric.includes(character))
+    .join('');
+
+    const reverseWord = cleanWord.split('').reverse().join('');
+    return reverseWord === cleanWord;
 };
 
 // Do not edit below this line
